435cb7e0bc9c64b54aeb1356aacb91670e3b3eb0
sporkmonger <sporkmonger>
2006-02-25 21:48:08 +0000

    Fixed plain text conversion.

--- lib/feed_tools/helpers/html_helper.rb
+++ lib/feed_tools/helpers/html_helper.rb
@@ -67,13 +67,13 @@ module FeedTools
       return nil if html.nil?
       stripped_html = html
       stripped_html = FeedTools::HtmlHelper.strip_html_tags(stripped_html)
+      stripped_html = FeedTools::HtmlHelper.unescape_entities(
+        FeedTools::HtmlHelper.unescape_entities(
+        FeedTools::HtmlHelper.unescape_entities(stripped_html)))
       stripped_html.gsub!(/&#8216;/, "'")
       stripped_html.gsub!(/&#8217;/, "'")
       stripped_html.gsub!(/&#8220;/, "\"")
       stripped_html.gsub!(/&#8221;/, "\"")
-      stripped_html = FeedTools::HtmlHelper.unescape_entities(
-        FeedTools::HtmlHelper.unescape_entities(
-        FeedTools::HtmlHelper.unescape_entities(stripped_html)))
       return stripped_html  
     end
     
